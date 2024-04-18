e48d80d4ff2f4343f3c8299f15c146a347b0ee10
AnneRom <coolann000.com@gmail.com>
2024-04-18 17:34:16 +0300

    Lesson18_04
    В JS прописали .addEventListener('mouseover') для елементу каталог, підключили до нього анімацію, прописали відключення анімації по її завершені, що при події mouseout каталог зникає. В стилах створили клас .catalog-anim  і анімацію slide для нього.

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 
 let items = document.querySelectorAll('.menu ul li');
-
 console.log(items);
+
+let item = document.querySelector(".menu ul li");
+console.log(item);
+
+item.addEventListener('mouseover', function() {
+    let catalogDiv = document.querySelector('.catalog');
+    catalogDiv.style.display = 'flex';
+    console.log(catalogDiv);
+
+    catalogDiv.classList.add('catalog-anim');
+    console.log(catalogDiv);
+
+    catalogDiv.addEventListener('animationend', function() {
+        catalogDiv.classList.remove('catalog-anim');
+        console.log(catalogDiv);
+    });
+});
+
+item.addEventListener('mouseout', function() {
+    let catalogDiv = document.querySelector('.catalog');
+    catalogDiv.style.display = 'none';
+    console.log(catalogDiv);
+});
+
