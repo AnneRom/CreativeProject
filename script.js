4acac4b48650e8ade993635fb447752542ea0619
AnneRom <coolann000.com@gmail.com>
2024-05-09 17:33:01 +0300

    Lesson-09.05
    Оптимізували код створили 3 функції:
    showCatalog,hideCatalog,hideAndshowCatalog. Додали нові функції в цикл замість минулого коду.

--- script.js
+++ script.js
@@ -1,69 +1,72 @@
-let items = document.querySelectorAll('.menu ul li');
-console.log(items);
+let items = document.querySelectorAll('.menu ul li');//[New Men Women Kids Acss Sale]
 
 let item = document.querySelector(".menu ul li");
-console.log(item);
-let isCatalogVisible = false;
+let isCatalogVisible = false;//Чи каталог видимий?
 let animationInProgress = false;//Чи вже триває анімація?
+let currentCatalogDiv = null;//буде визначати поточний каталог (той, на який курсор миші)
 
 // ! - НЕ
 // || - AБО
 // && - І
 
-/*console.log(false || true);//true
-console.log(false && true);//false
-console.log(true && true && false);//false
-console.log(true || true || false);//true
-console.log(true && true && true);//true*/
-
 items.forEach((itemList, index) => {
-    console.log(itemList, index);
 
-    let catalogDiv = document.getElementById(`catalog-${index + 1}`);
+    let catalogDiv = document.getElementById(`catalog-${index+1}`);
 
-    itemList.addEventListener('mouseover', function() {
+    itemList.addEventListener('mouseover', function () {
 
-        if (!isCatalogVisible && !animationInProgress) {// -> isCatalogVisible == false && animationInProgress == false
-
-        catalogDiv.style.display = 'flex';
-        isCatalogVisible = true;//Чи елемент каталог видимий?
-        console.log(catalogDiv);
-    
-        catalogDiv.classList.add('catalog-anim-in');
-        animationInProgress = true;
-        console.log(catalogDiv);
-    
-        catalogDiv.addEventListener('animationend', function doingAnimationEnd() {
-            catalogDiv.classList.remove('catalog-anim-in');
-            animationInProgress = false;
-            console.log(catalogDiv);
-    
-            catalogDiv.removeEventListener('animationend', doingAnimationEnd);
-        });  
-    }
+        if (!isCatalogVisible && !animationInProgress) {// -> isCatalogVisible == false
+            hideAndShowCatalog (catalogDiv);
+        }
+        else if (currentCatalogDiv !== catalogDiv) {
+            hideCatalog (catalogDiv);
+        }
     });
 
     itemList.addEventListener('mouseout', function() {
 
-        if (!item.matches(':hover') && isCatalogVisible && !animationInProgress) {// -> isCatalogVisible == true
-    
-        catalogDiv.classList.add('catalog-anim-out');
-        animationInProgress = true;
-        console.log(catalogDiv);
-    
-        catalogDiv.addEventListener('animationend', function doingAnimationEnd() {
-            catalogDiv.classList.remove('catalog-anim-out');
-            catalogDiv.style.display = 'none';
-            isCatalogVisible = false;
-            animationInProgress = false;
-            console.log(catalogDiv);
-    
-            catalogDiv.removeEventListener('animationend', doingAnimationEnd);
-        });
+    if(!itemList.matches(':hover') && isCatalogVisible && !animationInProgress) {// -> isCatalogVisible == true
+        hideCatalog(currentCatalogDiv);
     }
-        
-    });
-
+    
+});
 });
 
 
+function hideAndShowCatalog (catalogDiv) { //catalogDiv - це параметр функції(поки щось невідоме)
+    if (currentCatalogDiv !==null) {
+        hideCatalog (currentCatalogDiv);//при виклику функції currentCatalogDiv - це аргумент функції(конкретне значення)
+    }
+    showCatalog (catalogDiv);
+}
+
+function showCatalog (catalogDiv) {
+    catalogDiv.style.display = 'flex';
+    isCatalogVisible = true;
+    
+    catalogDiv.classList.add('catalog-anim-in');
+    animationInProgress = true;
+
+    catalogDiv.addEventListener('animationend', function doingAnimationEnd() {
+        catalogDiv.classList.remove('catalog-anim-in');
+        animationInProgress = false;
+        
+        catalogDiv.removeEventListener('animationend', doingAnimationEnd);
+    }); 
+    
+    currentCatalogDiv = catalogDiv;
+}
+
+function hideCatalog (catalogDiv) {
+    catalogDiv.classList.add('catalog-anim-out');
+    animationInProgress = true;
+
+    catalogDiv.addEventListener('animationend', function doingAnimationEnd() {
+        catalogDiv.classList.remove('catalog-anim-out');
+        catalogDiv.style.display = 'none';
+        isCatalogVisible = false;
+        animationInProgress = true;
+
+        catalogDiv.removeEventListener('animationend', doingAnimationEnd);
+    });
+}
