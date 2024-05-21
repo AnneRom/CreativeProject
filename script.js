13c390be559b2c0523efa93004f61c981db3ec7a
AnneRom <coolann000.com@gmail.com>
2024-05-21 17:42:19 +0300

    Lesson_21-05
    Почистили код: оновили три функції hideCatalog, hideAndshowCatalog, showCatalog. До функцій hideCatalog, showCatalog додали параметр withAnimation - який перевіряє чи потрібно додавати анімацію. Ініціалізували змінну previousCatalogDiv. Оновили логіку в основносму коді.

--- script.js
+++ script.js
@@ -1,45 +1,50 @@
 let items = document.querySelectorAll('.menu ul li');
 console.log(items);
 
+let item = document.querySelector(".menu ul li");
+console.log(item);
 let isCatalogVisible = false;//Чи каталог видимий
 let animationInProgress = false;//Чи вже триває анімація?
-let currentCatalogDiv = null;//буде визначати поточний каталог (той, на який курсор миші)
-let showCatalogTimer = null; // змінна для зберігання таймеру перед викликом функції showCatalog()
 
-let menuContainer = document.querySelector('.menu ul');
-
-menuContainer.addEventListener('mouseleave', function () {
-    hideCatalog(currentCatalogDiv, true);
-});
+let previousCatalogDiv = null;//відповідає за зберігання попередгього каталогу(той, на який було наведено курсор миші до цього)
+let hideCatalogTimer = null;//змінна для зберігання таймеру перед викликом функції hideCatalog()
+// ! - НЕ
+// || - AБО
+// && - І
+//комбінація клавіш щоб закоментувати/розкоментувати виділений код -> ctrl + /
 
 items.forEach((itemList, index) => {
     let catalogDiv = document.getElementById(`catalog-${index+1}`);
     
+    // let relatedItem = itemList.relatedTarget
+    // let targetItem = itemList.target;
+
     itemList.addEventListener('mouseenter', function () {
-        clearTimeout(showCatalogTimer);
-        
-        if (!isCatalogVisible && !animationInProgress) { // вказує на те, що жоден каталог не відкритий.
-            console.log("eeee");
 
-            showCatalogTimer = setTimeout(() => {
-                showCatalog(catalogDiv, true);
-            }, 200); // Затримка перед показом каталогу
+        //clearTimeout(hideCatalogTimer);
 
-        } else if (currentCatalogDiv !== catalogDiv) {
-            showCatalogTimer = setTimeout(() => {
-                hideAndshowCatalog(catalogDiv);
-            }, 200); // Затримка перед показом каталогу
-          
-        }   
+        if (!isCatalogVisible && !animationInProgress) {// -> isCatalogVisible == false
+            showCatalog(catalogDiv, true);
+        }
+        else if (previousCatalogDiv !== catalogDiv) {
+            hideAndshowCatalog(catalogDiv);
+        }
     });
 
     itemList.addEventListener('mouseleave', function () {
-        clearTimeout(showCatalogTimer);
+
     });
 });
 
+
+//isCatalogVisible = true;
+
 function showCatalog(catalogDiv, withAnimation) {
-    if (withAnimation) {
+    console.log("showCatalog");
+
+    if (withAnimation){
+        //(withAnimation) => withAnimation == true 
+        //(!withAnimation) => withAnimation == false
         catalogDiv.style.display = 'flex';
         catalogDiv.classList.add('catalog-anim-in');
         isCatalogVisible = true;
@@ -48,43 +53,46 @@ function showCatalog(catalogDiv, withAnimation) {
         catalogDiv.addEventListener('animationend', function doingAnimationEnd() {
             catalogDiv.classList.remove('catalog-anim-in');
             animationInProgress = false;
-
+               
             catalogDiv.removeEventListener('animationend', doingAnimationEnd);
-        }); 
+            });
     } else {
         catalogDiv.style.display = 'flex';
-        isCatalogVisible = true;
-        animationInProgress = false;
+        isCatalogVisible = true;//Чи елемент каталог видимий? 
+        animationInProgress = false;//Чи працює анімація?
     }
-    currentCatalogDiv = catalogDiv;
+
+    previousCatalogDiv = catalogDiv;//зберігає попередний каталог
 }
 
 function hideAndshowCatalog(catalogDiv) {
-    console.log("aaaaaaa")
-    if (currentCatalogDiv !== null) {
-        hideCatalog(currentCatalogDiv, false);
+    //catalogDiv - це параметр функції(поки щось невідоме)
+    //при виклику функції currentCatalogDiv - це аргумент функції(конкретне значення)
+    if (previousCatalogDiv !== null) {
+        hideCatalog(previousCatalogDiv, false);
     }
     showCatalog(catalogDiv, false);
 }
 
 function hideCatalog(catalogDiv, withAnimation) {
-    if (catalogDiv && isCatalogVisible && !animationInProgress) {
-    if (withAnimation) {
+    console.log("hideCatalog");
+
+    if (isCatalogVisible && !animationInProgress) {// -> isCatalogVisible == true
+        if (withAnimation){
         catalogDiv.classList.add('catalog-anim-out');
         animationInProgress = true;
-   
+       
         catalogDiv.addEventListener('animationend', function doingAnimationEnd() {
             catalogDiv.classList.remove('catalog-anim-out');
             catalogDiv.style.display = 'none';
             isCatalogVisible = false;
             animationInProgress = false;
-    
             catalogDiv.removeEventListener('animationend', doingAnimationEnd);
         });
-    } else {
-        catalogDiv.style.display = 'none';
-        isCatalogVisible = false;
-        animationInProgress = false;
+        } else {
+            catalogDiv.style.display = 'none';
+            isCatalogVisible = false;
+            animationInProgress = false;
+        }    
     }
-}   
 }
