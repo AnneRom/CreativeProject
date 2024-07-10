fc5b646d0fa3bd89c7f586a2d5fd9838776ae188
AnneRom <coolann000.com@gmail.com>
2024-07-10 15:09:58 +0300

    Lesson_10-07
    Pop - видаляє останній елемент масиву та повертає його. Push - додає елемент в кінець масиву. shift - видаляє перший елемент масиву. unshift - додає перший елемент масиву.

--- scriptNotes.js
+++ scriptNotes.js
@@ -43,17 +43,59 @@ console.log(fruits.length);//0
 function showArray (){
 //цикл, який виводить всі елменти масиву
  for (let i = 0; i < fruits.length; i++) {
-   console.log(fruits[i]);
+   console.log(i, fruits[i]);
    //console.log(fruits.at(i));
 }
 }
 
 //останній елемент масиву
 console.log(fruits[fruits.length - 1]);//6
-console.log("At:", fruits.at(-1));//at можу мати від'ємні значення
+console.log("At:", fruits.at(-1));
+//at може мати від'ємні значення
+//at дозволяє отримати елемент масиву за індексом
+// console.log("[]", fruits[1]);
+// console.log("[]", fruits[5]);
+// console.log("[]", fruits[fruits.length - 1]);//6
+
+// console.log("[]", fruits[3]);
+// console.log("{}", fruits.at(-4));
+// console.log("{}", fruits.at(3));
 
 //передостанній елемент масиву
 console.log(fruits[fruits.length - 2]);//5
 console.log("At:", fruits.at(-2));
 
+//showArray();
+
+//Pop - видаляє останній елемент масиву та повертає його
+console.log("Pop:");
+console.log(fruits.pop(), "видалено");
 showArray();
+
+console.log(fruits.pop(), "видалено");
+showArray();
+
+//Push - додає елмент в кінець масиву
+console.log("Push:");
+console.log(fruits.push("Peach"), "додано");
+showArray();
+
+fruits.push("Cherry");
+fruits.push("Apricot");
+
+console.log(fruits);
+
+//showArray();
+
+fruits.pop();
+fruits.pop();
+fruits.push("Lemon");
+console.log(fruits);
+
+//shift - видаляє перший елемент масиву
+fruits.shift();
+console.log(fruits);
+
+//unshift - додає перший елемент масиву
+fruits.unshift("Apple");
+console.log(fruits);
