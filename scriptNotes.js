8a3a2151389182bcc86db4da7aaf55dd7bf515d0
AnneRom <coolann000.com@gmail.com>
2024-07-11 15:38:34 +0300

    Lesson_11-07
    //push/pop працюють швидко
    //shift/unshift, працюють повільно; цикли і масиви; створили цикл for, який окремо рахує суму парних/непарних чисел, суму всіх чисел, факторіал всіх чисел масиву; цикл for..of.

--- scriptNotes.js
+++ scriptNotes.js
@@ -89,7 +89,7 @@ console.log(fruits);
 
 fruits.pop();
 fruits.pop();
-fruits.push("Lemon");
+fruits.push("Lemon", "Pinneapple");
 console.log(fruits);
 
 //shift - видаляє перший елемент масиву
@@ -97,5 +97,44 @@ fruits.shift();
 console.log(fruits);
 
 //unshift - додає перший елемент масиву
-fruits.unshift("Apple");
+fruits.unshift("Apple", "Bluberry");
 console.log(fruits);
+//push/pop працюють швидко
+//shift/unshift, працюють повільно.
+
+//Цикли
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let sumEven = 0, sumOdd = 0;
+let factorial = 1;
+
+console.log(numbers);
+
+for (let i = 0; i < numbers.length; i++){
+  //console.log(numbers[i]);
+    if (numbers[i] % 2 == 0){//numbers[i]%2 - остача від ділення на 2
+        sumEven = sumEven + numbers[i];
+    } else {
+        sumOdd = sumOdd + numbers[i];
+    }
+    factorial = factorial * numbers[i];
+}
+console.log('Sum of even numbers:', sumEven);
+console.log('Sum of odd numbers:', sumOdd);
+console.log('Factorial:', factorial);
+
+// for...of
+sumEven = 0;
+sumOdd = 0;
+factorial = 1;
+for (let num of numbers){
+    console.log(num);
+    if (num % 2 == 0){//numbers[i]%2 - остача від ділення на 2
+        sumEven = sumEven + num;
+    } else {
+        sumOdd = sumOdd + num;
+    }
+    factorial = factorial * num;
+}
+console.log('Sum of even numbers:', sumEven);
+console.log('Sum of odd numbers:', sumOdd);
+console.log('Factorial:', factorial);
